0464fe9945912b76d934173dff69963b21780b40
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-02-16 07:25:02 -0500

    Create .rb

---
+++ .rb
@@ -0,0 +1,32 @@
+class Account
+attr_reader :name, :balance
+  
+  def initialize(name, balance=100)
+    @name = name
+    @balance = balance
+  end
+  # public method that displays the user's balance if the correct pin is entered.
+  def display_balance(pin_number)
+    puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
+  end  
+ # public method that initiates cash withdrawal if the pin is correct.
+  def withdraw(pin_number, amount)
+      if pin_number == pin
+        @balance -= amount
+        puts "Withdrew #{amount}. New balance: $#{@balance}."
+      else 
+          pin_error
+      end
+  end
+  #private method for pin entry
+  private
+  def pin
+    @pin = 1234
+  end
+
+  def pin_error
+    "Access denied: incorrect PIN."
+  end
+end
+
+checking_account = Account.new("Jacob", 100 )
